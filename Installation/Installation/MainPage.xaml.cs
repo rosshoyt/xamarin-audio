e40879794581dab2a3a2470e8b34b72261d06d3a
Ross Hoyt <rosshoytmusic@gmail.com>
2021-01-11 15:12:20 -0800

    Implemented simple sound-playing button

--- Installation/Installation/MainPage.xaml.cs
+++ Installation/Installation/MainPage.xaml.cs
@@ -4,22 +4,58 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
 using Xamarin.Forms;
+using Plugin.SimpleAudioPlayer;
 
+// Simple sound implementation based on the approach described here:
+// https://devblogs.microsoft.com/xamarin/adding-sound-xamarin-forms-app/
 namespace Installation
-{
+{   
+    public enum SoundType
+    {
+        ButtonClickTest,
+        count
+    }
+
     public partial class MainPage : ContentPage
     {
+        ISimpleAudioPlayer[] players = new ISimpleAudioPlayer[(int)SoundType.count];
+
         public MainPage()
         {
             InitializeComponent();
+            SetupSounds();
+        }
+        void SetupSounds()
+        {
+            for (int i = 0; i < (int)SoundType.count; i++)
+            {
+                players[i] = CrossSimpleAudioPlayer.CreateSimpleAudioPlayer();
+                players[i].Loop = false;
+            }
+            LoadSounds();
+        }
+
+        void LoadSounds()
+        {
+            // TODO replace hardcoded Solution and Folder names
+            players[(int)SoundType.ButtonClickTest].Load(GetStreamFromFile("Installation.Audio.BtnClick.wav"));
+        }
+
+        Stream GetStreamFromFile(string filename)
+        {
+            var assembly = typeof(App).GetTypeInfo().Assembly;
+            var stream = assembly.GetManifestResourceStream(filename);
+
+            return stream;
         }
 
         // TODO - make async?
         void OnButtonClicked(object sender, EventArgs args)
         {
-            Button button = sender as Button;
-            button.Text = "Button Pressed!"; 
+            players[0].Play();
         }
     }
 }
